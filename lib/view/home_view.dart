0ed0c13503e6c348f3b82a69d4945433becfb87a
Ethan-OH <osm6227@gmail.com>
2024-01-11 16:20:14 +0900

    원복

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -6,7 +6,6 @@ import 'package:dicom_image_control_app/component/my_appbar.dart';
 import 'package:dicom_image_control_app/component/search_button.dart';
 import 'package:dicom_image_control_app/model/study_tab.dart';
 import 'package:dicom_image_control_app/view/drawer.dart';
-import 'package:dicom_image_control_app/view/test.dart';
 import 'package:dicom_image_control_app/view/thumbnail_view.dart';
 import 'package:dicom_image_control_app/view_model/home_vm.dart';
 import 'package:flutter/material.dart';
@@ -68,87 +67,86 @@ class MainView extends StatelessWidget {
                         ),
                         ElevatedButton.icon(
                           onPressed: () {
-                            Get.dialog(TableCalendarWidget());
-                            // Get.dialog(Dialog(
-                            //   child: SizedBox(
-                            //     width: MediaQuery.of(context).size.width * 0.4,
-                            //     height:
-                            //         MediaQuery.of(context).size.height * 0.6,
-                            //     child: Padding(
-                            //       padding: const EdgeInsets.all(15.0),
-                            //       child: Column(
-                            //         mainAxisAlignment: MainAxisAlignment.center,
-                            //         crossAxisAlignment:
-                            //             CrossAxisAlignment.start,
-                            //         children: [
-                            //           TableCalendar(
-                            //             focusedDay: DateTime.now(),
-                            //             firstDay: DateTime.utc(2022, 1, 1),
-                            //             lastDay: DateTime.utc(2024, 12, 31),
-                            //             selectedDayPredicate: (day) =>
-                            //                 isSameDay(homeVM.selectedDay, day),
-                            //             onDaySelected:
-                            //                 (selectedDay, focusedDay) {
-                            //               //
-                            //             },
-                            //           ),
-                            //           const HomeTitle(title: '검사일자'),
-                            //           Row(
-                            //             mainAxisAlignment:
-                            //                 MainAxisAlignment.spaceAround,
-                            //             children: [
-                            //               TextButton.icon(
-                            //                 onPressed: () async {
-                            //                   homeVM.startDay =
-                            //                       await homeVM.updateDatePicker(
-                            //                           context, homeVM.startDay);
-                            //                   // setState(() {});
-                            //                 },
-                            //                 icon: const Icon(
-                            //                     Icons.calendar_month_outlined),
-                            //                 label: Text(
-                            //                   DateFormat('yyyy.MM.dd')
-                            //                       .format(homeVM.startDay),
-                            //                   style:
-                            //                       const TextStyle(fontSize: 20),
-                            //                 ),
-                            //               ),
-                            //               TextButton.icon(
-                            //                 onPressed: () async {
-                            //                   homeVM.endDay =
-                            //                       await homeVM.updateDatePicker(
-                            //                           context, homeVM.endDay);
-                            //                   // setState(() {});
-                            //                 },
-                            //                 icon: const Icon(
-                            //                     Icons.calendar_month_outlined),
-                            //                 label: Text(
-                            //                   DateFormat('yyyy.MM.dd')
-                            //                       .format(homeVM.endDay),
-                            //                   style:
-                            //                       const TextStyle(fontSize: 20),
-                            //                 ),
-                            //               ),
-                            //             ],
-                            //           ),
-                            //           Row(
-                            //             mainAxisAlignment:
-                            //                 MainAxisAlignment.center,
-                            //             children: [
-                            //               ElevatedButton(
-                            //                 onPressed: () {
-                            //                   // 검색
-                            //                   Get.back();
-                            //                 },
-                            //                 child: const Text('확인'),
-                            //               ),
-                            //             ],
-                            //           )
-                            //         ],
-                            //       ),
-                            //     ),
-                            //   ),
-                            // ));
+                            Get.dialog(Dialog(
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.6,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(15.0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      TableCalendar(
+                                        focusedDay: DateTime.now(),
+                                        firstDay: DateTime.utc(2022, 1, 1),
+                                        lastDay: DateTime.utc(2024, 12, 31),
+                                        selectedDayPredicate: (day) =>
+                                            isSameDay(homeVM.selectedDay, day),
+                                        onDaySelected:
+                                            (selectedDay, focusedDay) {
+                                          //
+                                        },
+                                      ),
+                                      const HomeTitle(title: '검사일자'),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          TextButton.icon(
+                                            onPressed: () async {
+                                              homeVM.startDay =
+                                                  await homeVM.updateDatePicker(
+                                                      context, homeVM.startDay);
+                                              // setState(() {});
+                                            },
+                                            icon: const Icon(
+                                                Icons.calendar_month_outlined),
+                                            label: Text(
+                                              DateFormat('yyyy.MM.dd')
+                                                  .format(homeVM.startDay),
+                                              style:
+                                                  const TextStyle(fontSize: 20),
+                                            ),
+                                          ),
+                                          TextButton.icon(
+                                            onPressed: () async {
+                                              homeVM.endDay =
+                                                  await homeVM.updateDatePicker(
+                                                      context, homeVM.endDay);
+                                              // setState(() {});
+                                            },
+                                            icon: const Icon(
+                                                Icons.calendar_month_outlined),
+                                            label: Text(
+                                              DateFormat('yyyy.MM.dd')
+                                                  .format(homeVM.endDay),
+                                              style:
+                                                  const TextStyle(fontSize: 20),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              // 검색
+                                              Get.back();
+                                            },
+                                            child: const Text('확인'),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ));
                           },
                           icon: const Icon(Icons.calendar_month),
                           label: const Text('날짜 선택'),
