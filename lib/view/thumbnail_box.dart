1cb279f6b70f7b2c800194769c13a9bb4dc73458
Ethan-OH <osm6227@gmail.com>
2024-01-29 17:39:06 +0900

    썸네일 이미지 요청받기 -> 이미 다운받은 이미지 보여주기 수정

--- lib/view/thumbnail_box.dart
+++ lib/view/thumbnail_box.dart
@@ -1,11 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:data_table_2/data_table_2.dart';
-import 'package:dicom_image_control_app/data/shared_handler.dart';
+import 'package:dicom_image_control_app/data/search_data.dart';
 import 'package:dicom_image_control_app/model/series_tab.dart';
 import 'package:dicom_image_control_app/model/study_tab.dart';
 import 'package:dicom_image_control_app/view/detail_view.dart';
 import 'package:dicom_image_control_app/view_model/detail_vm.dart';
-import 'package:dicom_image_control_app/view_model/thumbnail_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,7 +19,6 @@ class ThumbnailBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var controller = Get.find<ThumbnailVM>();
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -200,28 +197,29 @@ class ThumbnailBox extends StatelessWidget {
           SizedBox(
             width: 400,
             height: 400,
-            child: CachedNetworkImage(
-              fit: BoxFit.fitHeight,
-              // 요청 url
-              imageUrl: controller.generateThumbnailImageUrl(
-                seriesList: seriesList,
-                index: index,
-              ),
-              // 헤더에 토큰 담기
-              httpHeaders: {
-                'accept': 'application/json',
-                'Authorization': 'Bearer ${controller.token.value}'
-              },
+            child: Image.asset('$filePath/study_${study.STUDYKEY}/${study.STUDYKEY}_${seriesList[index].SERIESKEY}_1_1.png'),
+            // child: CachedNetworkImage(
+            //   fit: BoxFit.fitHeight,
+            //   // 요청 url
+            //   imageUrl: controller.generateThumbnailImageUrl(
+            //     seriesList: seriesList,
+            //     index: index,
+            //   ),
+            //   // 헤더에 토큰 담기
+            //   httpHeaders: {
+            //     'accept': 'application/json',
+            //     'Authorization': 'Bearer ${controller.token.value}'
+            //   },
 
-              progressIndicatorBuilder: (context, url, downloadProgress) =>
-                  CircularProgressIndicator(value: downloadProgress.progress),
-              errorWidget: (context, url, error) {
-                return const Icon(Icons.error);
-              },
-              errorListener: (value) async =>
-                  // 에러 발생 시 엑세스토큰 다시 가져오기
-                  print(value),
-            ),
+            //   progressIndicatorBuilder: (context, url, downloadProgress) =>
+            //       CircularProgressIndicator(value: downloadProgress.progress),
+            //   errorWidget: (context, url, error) {
+            //     return const Icon(Icons.error);
+            //   },
+            //   errorListener: (value) async =>
+            //       // 에러 발생 시 엑세스토큰 다시 가져오기
+            //       print(value),
+            // ),
           ),
         ],
       ),
