935efb1f2041b7ae33bd38ed851788bfabc5064e
Ethan-OH <osm6227@gmail.com>
2024-02-08 18:10:25 +0900

    디테일 뷰 재구성

--- lib/view/detail_view/detail_view.dart
+++ lib/view/detail_view/detail_view.dart
@@ -18,108 +18,117 @@ class DetailView extends GetView<DetailVM> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(
-          title: 'Detail View',
-          bottom: AppBar(
-            toolbarHeight: 100,
-            automaticallyImplyLeading: false,
-            title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GetBuilder<DetailVM>(
-                    builder: (_) => Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ToolbarButton(
-                          width: 83,
-                          icon: Icons.zoom_in,
-                          label: 'ZOOM',
-                          isSelected: _.isZoomSelected,
-                          onPressed: () => _.clickZoomButton(),
-                        ),
-                        ToolbarButton(
-                          icon: Icons.threesixty_outlined,
-                          label: '회전',
-                          isSelected: _.isRotationSelected,
-                          onPressed: () => _.clickRotationButton(),
-                        ),
-                        ToolbarButton(
-                          icon: Icons.flip,
-                          label: '반전',
-                          isSelected: _.isInvertionSelected,
-                          onPressed: () => _.invertColor(),
-                        ),
-                        ToolbarButton(
-                          icon: Icons.light_mode_rounded,
-                          label: '밝기',
-                          isSelected: _.isBrightSelected,
-                          onPressed: () => _.clickBrightnessButton()
-                        ),
-                        ToolbarButton(
-                          icon: Icons.brightness_6_outlined,
-                          label: 'WC',
-                          isSelected: _.isWCSelected,
-                          onPressed: () => _.clickWCButton()
-                        ),
-                      ],
+      appBar: MyAppBar(
+        title: 'Detail View',
+        bottom: AppBar(
+          toolbarHeight: 100,
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GetBuilder<DetailVM>(
+                builder: (_) => Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ToolbarButton(
+                      width: 83,
+                      icon: Icons.zoom_in,
+                      label: 'ZOOM',
+                      isSelected: _.isZoomSelected,
+                      onPressed: () => _.clickZoomButton(),
                     ),
-                  ),
-                  const SizedBox(width: 100,),
-                  const BrightSlider(),
-                  const WindowCenterSlider(),
-                ],
+                    ToolbarButton(
+                      icon: Icons.threesixty_outlined,
+                      label: '회전',
+                      isSelected: _.isRotationSelected,
+                      onPressed: () => _.clickRotationButton(),
+                    ),
+                    ToolbarButton(
+                      icon: Icons.flip,
+                      label: '반전',
+                      isSelected: _.isInvertionSelected,
+                      onPressed: () => _.invertColor(),
+                    ),
+                    ToolbarButton(
+                        icon: Icons.light_mode_rounded,
+                        label: '밝기',
+                        isSelected: _.isBrightSelected,
+                        onPressed: () => _.clickBrightnessButton()),
+                    ToolbarButton(
+                        icon: Icons.brightness_6_outlined,
+                        label: 'WC',
+                        isSelected: _.isWCSelected,
+                        onPressed: () => _.clickWCButton()),
+                  ],
+                ),
               ),
+              const SizedBox(
+                width: 100,
+              ),
+              // TODO: 위젯 합치기
+              const BrightSlider(),
+              const WindowCenterSlider(),
+            ],
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 730,
-              child: ClipRRect(
-                child: GetBuilder<DetailVM>(
-                  builder: (_) => ColorFiltered(
-                    colorFilter: ColorFilter.matrix([
-                      _.brightValue, 0.0, 0.0, 0.0,
-                      _.isConverted ? 255 : 0,
-                      0.0, _.brightValue, 0.0, 0.0,
-                      _.isConverted ? 255 : 0,
-                      0.0, 0.0, _.brightValue, 0.0,
-                      _.isConverted ? 255 : 0,
-                      0.0, 0.0, 0.0, 1.0, 0.0, //
-                    ]),
-                    child: GestureDetector(
-                      onVerticalDragUpdate: (details) => controller.changeImage(details, series.IMAGECNT),
-                      child: PhotoView(
-                        enableRotation: controller.isRotationSelected, // 회전 기능,
-                        disableGestures: !controller.isZoomSelected,
-                        scaleStateController: controller.scaleStateController,
-                        filterQuality: FilterQuality.high,
-                        backgroundDecoration:
-                            const BoxDecoration(color: Colors.black),
-                        imageProvider: AssetImage(
-                            '$filePath/study_$studyKey/${studyKey}_${series.SERIESKEY}_${controller.imageIndex}_${controller.windowCenterIndex}.png'),
-                      ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 730,
+            child: ClipRRect(
+              child: GetBuilder<DetailVM>(
+                builder: (_) => ColorFiltered(
+                  colorFilter: ColorFilter.matrix([
+                    _.brightValue, 0.0, 0.0, 0.0,
+                    _.isConverted ? 255 : 0,
+                    0.0, _.brightValue, 0.0, 0.0,
+                    _.isConverted ? 255 : 0,
+                    0.0, 0.0, _.brightValue, 0.0,
+                    _.isConverted ? 255 : 0,
+                    0.0, 0.0, 0.0, 1.0, 0.0, //
+                  ]),
+                  child: GestureDetector(
+                    onVerticalDragUpdate: (details) =>
+                        controller.changeImage(details, series.IMAGECNT),
+                    child: PhotoView(
+                      enableRotation: controller.isRotationSelected, // 회전 기능,
+                      disableGestures: !controller.isZoomSelected,
+                      scaleStateController: controller.scaleStateController,
+                      filterQuality: FilterQuality.high,
+                      backgroundDecoration:
+                          const BoxDecoration(color: Colors.black),
+                      imageProvider: AssetImage(
+                          '$filePath/study_$studyKey/${studyKey}_${series.SERIESKEY}_${controller.imageIndex}_${controller.windowCenterIndex}.png'),
                     ),
                   ),
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    height: 100,
-                    child: (series.IMAGECNT != 1)
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text('이미지 이동', style: TextStyle(fontSize: 25),),
-                              SizedBox(
-                                width: 500,
-                                child: GetBuilder<DetailVM>(
-                                  builder: (_) => Slider(
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 100,
+                  child: (series.IMAGECNT != 1)
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              '이미지 이동',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            SizedBox(
+                              width: 500,
+                              child: GetBuilder<DetailVM>(
+                                builder: (_) => SliderTheme(
+                                  data: const SliderThemeData(
+                                    trackHeight: 20,
+                                  ),
+                                  child: Slider(
                                     value: _.imageIndex.toDouble(),
                                     label: _.imageIndex.toString(),
                                     min: 1,
@@ -132,13 +141,14 @@ class DetailView extends GetView<DetailVM> {
                                   ),
                                 ),
                               ),
-                            ],
-                          )
-                        : const SizedBox()),
-              ],
-            )
-          ],
-        ),
-      );
+                            ),
+                          ],
+                        )
+                      : const SizedBox()),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
