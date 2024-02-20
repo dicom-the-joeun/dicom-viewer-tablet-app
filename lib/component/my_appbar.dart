82e0d5a32601cf5867328209a775f54e5c1c5af7
Ethan-OH <osm6227@gmail.com>
2024-02-20 16:45:34 +0900

    appbar 수정

--- lib/component/my_appbar.dart
+++ lib/component/my_appbar.dart
@@ -23,3 +23,36 @@ PreferredSizeWidget MyAppBar({required String title, List<Widget>? actions, Pref
       actions: actions,
       bottom: bottom,
     );
+    
+// ignore: non_constant_identifier_names
+PreferredSizeWidget MyHomeAppBar({required String title, required String logoPath, List<Widget>? actions, PreferredSizeWidget? bottom}) =>
+    AppBar(
+      iconTheme: const IconThemeData(
+        size: 30,
+      ),
+      toolbarHeight: 70,
+      backgroundColor: Colors.black,
+      title: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+                child: Image.asset(logoPath),
+              ),
+              SizedBox(width: 10,),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: actions,
+      bottom: bottom,
+    );
